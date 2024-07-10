4aab8b838841deb30992acc4d6523e585afe9893
milorb <mrbaumhardt@gmail.com>
2024-07-10 03:04:01 -0400

    cleaned up the basic stuff: concat, and get. smart pointer-ified the nodes

--- ropes.cpp
+++ ropes.cpp
@@ -1,27 +1,148 @@
 #include "ropes.hpp"
 
-Rope::Rope(const std::string &s) {
-  weave(s, 5);
+                  // *********** Rope public *********** //
+
+void Rope::concat(const Rope& other) {
+  root->_concat(*other.root);
 }
 
-void Rope::weave(const std::string& s, int str_max) {
+char Rope::get(int idx) {
+  return root->_get_char(idx);
+}
+
+void Rope::insert_elt(char c, int idx) {
+
+}
+
+void Rope::print() {
+  root->_print();
+}
+
+size_t Rope::size() {
+  return root->k_weight;
+}
+
+                  // *********** Rope constructors *********** //
+
+Rope::Rope(std::string& s, int max) {
+    root->weave(s, max);
+}
+
+Rope::Rope(const std::string& s, int max) {
+    root->weave(s, max);
+}
+
+
+                  // *********** Rope_t private *********** //
+
+void Rope_t::weave(const std::string& s, int str_max) {
   if (s.size() <= str_max) {
-    knot.str = s;
+    weight = s.size();
+    k_weight = weight;
+    knot.tied = true;
+    knot.conts = s;
     return;
   }
-  
+
+  std::shared_ptr<Rope_t> self = shared_from_this();
+
   size_t mid = s.size() / 2;
-  weight = mid;
   std::string ls(s.begin(), s.begin() + mid);
   std::string rs(s.begin() + mid, s.end());
 
-  left = new Rope(ls);
-  right = new Rope(rs);
-  left->parent = this;
-  right->parent = this;
+  k_weight = s.size();
+  weight = ls.size();
+
+  right = std::make_shared<Rope_t>();
+  left = std::make_shared<Rope_t>();
+  
+  right->weave(rs, str_max);
+  left->weave(ls, str_max);
+
+  right->parent = self;
+  left->parent = self;
 }
 
-Rope::~Rope() {
-  delete left;
-  delete right;
+std::shared_ptr<Rope_t> Rope_t::copy(
+  std::shared_ptr<Rope_t> r, std::weak_ptr<Rope_t> p) {
+  if (!r) {
+    return nullptr;
+  }
+  auto Rope_t_new = std::make_shared<Rope_t>();
+
+  Rope_t_new->parent = p;
+
+  Rope_t_new->knot = r->knot;
+  Rope_t_new->weight = r->weight; 
+  Rope_t_new->k_weight = r->k_weight;
+
+  Rope_t_new->left = copy(r->left, Rope_t_new);
+  Rope_t_new->right = copy(r->right, Rope_t_new);
+  return Rope_t_new;
+}
+
+                  // *********** Rope_t public *********** //
+
+void Rope_t::_concat(const Rope_t& r) {
+  auto temp = std::make_unique<Rope_t>(*this);
+
+  if (left) {
+    weight = left->k_weight;
+  } else {
+    std::cout << 
+          "error: attempt to call concatenate on empty Rope_t" 
+                                                          << std::endl;
+    std::exit(1);
+  }
+
+  k_weight = k_weight + r.k_weight;
+  right = std::make_shared<Rope_t>(r);
+
+  left = std::move(temp);
+}
+
+char Rope_t::_get_char(int idx) {
+  if (idx >= k_weight) {
+    std::cout << 
+          "error: attempt to index into invalid range" 
+                                                  << std::endl;
+    std::exit(1);
+  }
+  if (knot.tied) {
+    assert(idx < knot.conts.size());
+    return knot.conts[idx];
+  }
+  if (idx < weight) {
+    return left->_get_char(idx);
+  } else { 
+    return right->_get_char(idx - weight);
+  }
+}
+
+void Rope_t::_insert_elt(char c, int idx) {
+  
+}
+
+void Rope_t::_print() {
+  if (!knot.conts.empty()) {
+    std::cout << knot.conts;
+    return;
+  } else {
+    if (left) {
+	    left->_print();
+    }
+    if (right) {
+	    right->_print();
+    }
+  }
+}
+
+                  // *********** Rope_t constructors *********** //
+
+Rope_t::Rope_t() {
+  right = nullptr;
+  left = nullptr;
+  knot = Knot<std::string>();
+  k_weight = 0;
+  weight = 0;
 }
--- ropes.hpp
+++ ropes.hpp
@@ -3,31 +3,55 @@
 
 #include <memory>
 #include <string>
-
+#include <cassert>
 #include <iostream>
 
+/*
+ data structure idea: ropes but all the leaf nodes are a linked list
+*/
+
+template <typename T>
 struct Knot {
-  Knot() : str("") {}
-  std::string str;
+  bool tied = false;
+  T conts;
+};
+
+class Rope_t : public std::enable_shared_from_this<Rope_t> {
+private:
+  // assembles tree with leaf->chars.size() <= str_max
+  std::shared_ptr<Rope_t> copy(std::shared_ptr<Rope_t> r, std::weak_ptr<Rope_t> p);
+
+  uint64_t weight;
+
+public:
+  Rope_t();
+
+  void weave(const std::string& s, const int str_max); 
+  void _concat(const Rope_t& r1);
+  char _get_char(int idx);
+  void _insert_elt(char c, int idx);
+  void _print();
+  
+  std::weak_ptr<Rope_t> parent;
+  std::shared_ptr<Rope_t> right;
+  std::shared_ptr<Rope_t> left;
+  Knot<std::string> knot;
+  size_t k_weight;
 };
 
 class Rope {
-public: 
-// leaf constructor
-  Rope() : left(nullptr), right(nullptr), knot(Knot()), weight(0) {}
-// primary constructorthis
-  Rope(const std::string &s);
-  ~Rope();
-
-  Rope* parent;
-  Rope* left;
-  Rope* right;
-  Knot knot;
-  uint64_t weight;
-
 private:
-// assembles tree with leaf->chars.size() <= str_max
-  void weave(const std::string& s, const int str_max);
+  std::shared_ptr<Rope_t> root = std::make_shared<Rope_t>();;
+public:
+  Rope() {}
+  Rope(std::string& s, int max);
+  Rope(const std::string& s, int max);
+
+  void concat(const Rope& other);
+  char get(int idx);
+  void insert_elt(char c, int idx);
+  void print();
+  size_t size();
 };
 
 #endif
